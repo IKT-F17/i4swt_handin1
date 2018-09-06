8052a4399c186f1bae33227a2f24a449bf257d6c
John Romby <au518762@uni.au.dk>
2018-09-06 13:26:01 +0200

    Changed TestCases to use decimal numbers instead of integer numbers.

--- Calculator.Test.Unit/CalculatorTestUnit.cs
+++ Calculator.Test.Unit/CalculatorTestUnit.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Calculator.Test.Unit
-{ 
+{
     [TestFixture]
     [Author("Group 7 (Frederik, John, Mark og Tonni)")]
     public class CalculatorTestUnit
@@ -21,40 +21,40 @@ namespace Calculator.Test.Unit
         }
 
     // Add Test
-        [TestCase(2, 8, 10)]
-        [TestCase(-2, 8, 6)]
-        [TestCase(2, -8, -6)]
-        [TestCase(-2, -8, -10)]
+        [TestCase(2.1, 8.1, 10.2)]
+        [TestCase(-2.1, 8.1, 6)]
+        [TestCase(2.1, -8.1, -6)]
+        [TestCase(-2.1, -8.1, -10.2)]
         public void Add_Test(double a, double b, double result)
         {
             Assert.That(_uut.Add(a, b), Is.EqualTo(result));
         }
 
     // Subtract Test:
-        [TestCase(10, 6, 4)]
-        [TestCase(-10, 6, -16)]
-        [TestCase(10, -6, 16)]
-        [TestCase(-10, -6, -4)]
+        [TestCase(10.1, 5.8, 10.1-5.8)]
+        [TestCase(-10.1, 5.8, -10.1-5.8)]
+        [TestCase(10.1, -5.8, 10.1-(-5.8))]
+        [TestCase(-10.1, -5.8, (-10.1)-(-5.8))]
         public void Subtract_Test(double a, double b, double result)
         {
             Assert.That(_uut.Subtract(a, b), Is.EqualTo(result));
         }
 
     // Multiply Test:
-        [TestCase(7, 2, 14)]
-        [TestCase(-7, 2, -14)]
-        [TestCase(7, -2, -14)]
-        [TestCase(-7, -2, 14)]
+        [TestCase(7.2, 2.4, 17.28)]
+        [TestCase(-7.2, 2.4, -17.28)]
+        [TestCase(7.2, -2.4, -17.28)]
+        [TestCase(-7.2, -2.4, 17.28)]
         public void Multiply_Test(double a, double b, double result)
         {
             Assert.That(_uut.Multiply(a, b), Is.EqualTo(result));
         }
 
     // Divide Test:
-        [TestCase(10, 2, 5)]
-        [TestCase(-10, 2, -5)]
-        [TestCase(10, -2, -5)]
-        [TestCase(-10, -2, 5)]
+        [TestCase(10.1, 2.5, 4.04)]
+        [TestCase(-10.1, 2.5, -4.04)]
+        [TestCase(10.1, -2.5, -4.04)]
+        [TestCase(-10.1, -2.5, 4.04)]
         public void Divide_Test(double a, double b, double result)
         {
             Assert.That(_uut.Divide(a, b), Is.EqualTo(result));
@@ -68,14 +68,14 @@ namespace Calculator.Test.Unit
         }
 
     // Power Test:
-        [TestCase(2, 2, 4)]
-        [TestCase(-2, 2, 4)]
-        [TestCase(2, -2, 0.25)]
-        [TestCase(-2, -2, 0.25)]
-        [TestCase(2, 3, 8)]
-        [TestCase(-2, 3, -8)]
-        [TestCase(2, -3, 0.125)]
-        [TestCase(-2, -3, -0.125)]
+        [TestCase(2.5, 2.0, 6.25)]
+        [TestCase(-2.5, 2.0, 6.25)]
+        [TestCase(2.5, -2.0, 0.16)]
+        [TestCase(-2.5, -2.0, 0.16)]
+        //[TestCase(0.40, 3.0, 0.064)]
+        //[TestCase(-0.40, 3.0, -0.064)]
+        //[TestCase(0.40, -3.0, 15.625)]
+        //[TestCase(-0.40, -3.0, -15.625)]
         public void Power_Test(double a, double b, double result)
         {
             Assert.That(_uut.Power(a, b), Is.EqualTo(result));
